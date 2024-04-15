1645cbe8bd229bec8b5b14207cd749ca69bde854
Tobias Langendorf <junglerobba@jngl.one>
2024-04-15 22:19:34 +0200

    auto upgrade: write lock file

--- configuration.nix
+++ configuration.nix
@@ -168,8 +168,7 @@
   system.autoUpgrade = {
     enable = true;
     flake = inputs.self.outPath;
-    flags =
-      [ "--update-input" "nixpkgs" "--no-write-lock-file" "--impure" "-L" ];
+    flags = [ "--update-input" "nixpkgs" "--impure" "-L" ];
     dates = "02:00";
     randomizedDelaySec = "45min";
   };
