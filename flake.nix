e25885e6dd77a928785844b273c56082a24623f3
Tobias Langendorf <junglerobba@jngl.one>
2024-07-07 12:26:17 +0200

    override inputs from home-config repo

--- flake.nix
+++ flake.nix
@@ -9,14 +9,25 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    tms = {
+      url = "github:jrmoulton/tmux-sessionizer";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    helix = {
+      url = "github:helix-editor/helix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-config = {
       url = "github:junglerobba/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
+      inputs.tms.follows = "tms";
+      inputs.helix.follows = "helix";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       desktop = "gnome";
@@ -25,9 +36,12 @@
         inherit username desktop;
         homedir = "/home/${username}";
       };
-    in {
+    in
+    {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs desktop username; };
+        specialArgs = {
+          inherit inputs desktop username;
+        };
         modules = [
           ./configuration.nix
           inputs.home-manager.nixosModules.default
