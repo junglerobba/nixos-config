e7c54189021d995140be9adecb6b5c5fd8872350
Tobias Langendorf <junglerobba@jngl.one>
2024-08-14 22:54:47 +0200

    cosmic: add some gnome apps
    probably some more to follow

--- configuration.nix
+++ configuration.nix
@@ -200,11 +200,20 @@ in
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    libsecret
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+      libsecret
+    ])
+    ++ lib.optionals cosmic (
+      with pkgs;
+      [
+        gnome-system-monitor
+        nautilus
+        alacritty
+      ]
+    );
 
   fonts.packages =
     (with pkgs; [
